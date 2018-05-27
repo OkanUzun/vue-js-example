5e1c1de7e23dbe0a59d76f3bbf3aa01e6f1b480e
Okan <o.uzun@reply.de>
2018-05-27 17:53:41 +0300

    Initial Commit

---
+++ src/components/Example.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{message}} {{firstName}} {{lastName}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Example',
+    data () {
+      return {
+        message: 'Hello',
+        firstName: 'Okan',
+        lastName: 'Uzun'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
